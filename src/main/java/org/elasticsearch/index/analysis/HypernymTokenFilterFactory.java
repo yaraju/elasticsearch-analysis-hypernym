8cc3c2daf4811b364b6e9554def00e5170e58405
Yerramilli Avanish Raju <araju@svabhu.org>
2015-06-05 22:00:18 +0530

    Fix the type of error thrown by token filter factory.

--- src/main/java/org/elasticsearch/index/analysis/HypernymTokenFilterFactory.java
+++ src/main/java/org/elasticsearch/index/analysis/HypernymTokenFilterFactory.java
@@ -3,7 +3,9 @@ package org.elasticsearch.index.analysis;
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.uima.lucas.indexer.analysis.HypernymFilterFactory;
 import org.apache.uima.lucas.indexer.util.MultimapFileReaderFactory;
+import org.elasticsearch.ElasticsearchIllegalArgumentException;
 import org.elasticsearch.common.inject.Inject;
+import org.elasticsearch.common.inject.assistedinject.Assisted;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.index.Index;
 import org.elasticsearch.index.settings.IndexSettings;
@@ -19,13 +21,13 @@ public class HypernymTokenFilterFactory extends org.elasticsearch.index.analysis
     private Properties properties;
 
     @Inject
-    public HypernymTokenFilterFactory(Index index, @IndexSettings Settings indexSettings, String name, Settings settings) {
+    public HypernymTokenFilterFactory(Index index, @IndexSettings Settings indexSettings, @Assisted String name, @Assisted Settings settings) {
         super(index, indexSettings, name, settings);
         filterFactoryDelegate = new HypernymFilterFactory(new MultimapFileReaderFactory());
         if (settings.get("hypernyms_path") != null) {
             properties.put(HypernymFilterFactory.FILE_PATH_PARAMETER, indexSettings.get("hypernyms_path"));
         } else {
-            throw new IllegalArgumentException("hypernym requires `hypernyms_path` to be configured");
+            throw new ElasticsearchIllegalArgumentException("hypernym requires `hypernyms_path` to be configured");
         }
 
     }
